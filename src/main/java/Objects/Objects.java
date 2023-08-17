c0a70ccc6b4cd1fbc1e6db8a450817e7d6e64920
karthickr96 <104310219+karthickr96@users.noreply.github.com>
2023-08-17 08:44:50 +0530

    Delete Objects.java

--- src/main/java/Objects/Objects.java
+++
@@ -1,9 +0,0 @@
-package Objects;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class Objects {
-	@FindBy(xpath="//textarea[@name='q']")
-	public WebElement searchBar;
-}
